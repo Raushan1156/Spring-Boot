14ead9d3b62c2ab34e6aade652fbedf40ed24aac
raushan1156 <iamraushankumar56@gmail.com>
2025-01-17 07:56:23 +0530

    Lambda rules

--- Java-classes/LambdaExp.java
+++ Java-classes/LambdaExp.java
@@ -1,6 +1,21 @@
 public class LambdaExp{
     public static void main(String[] args) {
 
+
+// Lambda is a anonymous function having
+// no name
+// no modifier
+// no return type
+
+// Reduces the number of line code.
+// >It is always and only used with functional interface.
+// >Supports paralled and sequential executaion support by passing behaivour as an argument in method
+// >use api effectively
+// >more readable and concise
+
+
+// No functional dependency-> no lambda
+
         test t1=() -> System.out.println("Hello World");
         t1.test();
         // Lambda expression to add two numbers
