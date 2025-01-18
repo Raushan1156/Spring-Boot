7c2ff2af9698724ec060bd60559b915f885a7460
raushan1156 <iamraushankumar56@gmail.com>
2025-01-18 10:19:30 +0530

    sequential & paralled stram():Reference operator

--- Java-classes/StreamApi/StreamApi.java
+++ Java-classes/StreamApi/StreamApi.java
@@ -1,4 +1,5 @@
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.List;
 
@@ -7,14 +8,27 @@ public class StreamApi{
         System.out.println("Hello World");
 
         List<Integer> list2= List.of(1,2,3,4,5,6,7,8,9,10);
+
+        // Sequential stream() is used for small data set with maintained order and single thread.
         Stream<Integer> stream2 = list2.stream();
         List<Integer> evenNumbers = stream2.filter(i -> i % 2 == 0).collect(Collectors.toList());
-        System.out.println(list2);
+        //System.out.println(list2);
+        list2.stream().forEach(System.out::println);
+        //System.out.println(list2.getClass());
         
-        // Create a new stream for the forEach operation
-//        list2.stream().forEach(System.out::println);
-        System.out.println(list2.getClass());
-        
-        System.out.println(evenNumbers);
+        //System.out.println(evenNumbers);
+        // Parallel stream() is used for large data set wit multi thread.
+        list2.parallelStream()
+                .filter(i->i%2==0)
+                .forEach(i->System.out.println(i));
+
+
+
+        // Example of large data set
+        int total= IntStream.rangeClosed(1,1_00_00_00)
+                .parallel()
+                .filter(i->i%2!=0)
+                .sum();
+        System.out.println(total);
     }
 }
